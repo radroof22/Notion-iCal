32a093f13fb6ea27189e94a4ab0a4c2223b1fe2b
Rohan <mehta.rohan@outlook.com>
2021-09-06 00:57:29 -0400

    Drafted Readme

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     load_dotenv()
     notion_token = os.getenv("NOTION_TOKEN")
     notion_client = NotionClient(notion_token)
-    notion_client.get_database("1690aa9ec9194d57bcf322b9f83dd105")
+    notion_client.get_database(os.getenv("DATABASE_ID"))
